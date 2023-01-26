9ab429385472590f78feaf5f3c90c722a5b1cbb1
Bruno Quintana <bruno.quintana@gmail.com>
2023-01-26 10:25:24 +0000

    Add fifo support

---
+++ django_sqs_extended_client/event/event_base_aws_fifo.py
@@ -0,0 +1,29 @@
+from django.conf import settings
+
+from ..aws.sns_client_extended import SNSClientExtended
+from .event_base import EventBase
+
+
+class EventBaseAwsFifo(EventBase):
+
+    AWS_ACCESS_KEY_ID = settings.AWS_ACCESS_KEY_ID
+    AWS_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
+    AWS_DEFAULT_REGION = settings.AWS_DEFAULT_REGION
+    AWS_S3_QUEUE_STORAGE_NAME = settings.AWS_S3_QUEUE_STORAGE_NAME
+    AWS_SNS_TOPIC_FIFO = settings.AWS_SNS_TOPIC_FIFO
+
+    def dispatch(self, event_name, event_data):
+        sns = SNSClientExtended(self.AWS_ACCESS_KEY_ID,
+                                self.AWS_SECRET_ACCESS_KEY,
+                                self.AWS_DEFAULT_REGION,
+                                self.AWS_S3_QUEUE_STORAGE_NAME)
+        return sns.send_message(
+            self.AWS_SNS_TOPIC_FIFO,
+            event_data,
+            message_attributes={
+                'event_type': {
+                    'DataType': 'String',
+                    'StringValue': event_name
+                }
+            }
+        )
---
+++ django_sqs_extended_client/event/event_dispatcher_fifo.py
@@ -0,0 +1,5 @@
+from django_sqs_extended_client.event.event_base_aws_fifo import EventBaseAwsFifo
+
+
+class EventDispatcherFifo(EventBaseAwsFifo):
+    pass
